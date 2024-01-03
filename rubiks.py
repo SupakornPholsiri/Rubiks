470c347df1dc7ea9f121e7af4a8f18d483a9636b
SupakornPholsiri <supakorncpw@gmail.com>
2024-01-03 21:32:36 +0700

    Change the unknown axis message

--- rubiks.py
+++ rubiks.py
@@ -125,4 +125,4 @@ if __name__ == "__main__":
         elif instruction[0] == "exit":
             break
         else:
-            print("bruh")
+            print(f"No known axis named {instruction[0]}")
